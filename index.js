b9e67b6fa7bf2ac848a33ce77c73b5afba7900c2
Andy Bettisworth <andy@accreu.com>
2019-05-27 09:34:24 -0500

    Install express and cors and setup server

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-// implement your API here
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const express = require('express')
+
+/**
+ * Constants
+ */
+
+const port = 8080
+
+/**
+ * Define app
+ */
+
+const app = express()
+
+/**
+ * Routes
+ */
+
+app.get('/', (req, res) => {
+  res.redirect('/api/users')
+})
+
+app.post('/api/users', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.get('/api/users', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.get('/api/users/:id', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.delete('/api/users/:id', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.put('/api/users/:id', (req, res) => {
+  res.sendStatus(200)
+})
+
+/**
+ * Start server
+ */
+
+if (module === require.main) {
+  console.log('Start server')
+  app.listen(port, () => {
+    console.log(`Express server running on ${port}`)
+  })
+}
+
+/**
+ * Export app
+ */
+
+module.exports = app
